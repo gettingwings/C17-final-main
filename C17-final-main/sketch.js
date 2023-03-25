1d55a66d65a7f67f52844152efa8dacdb8d09b95
gettingwings <74932161+gettingwings@users.noreply.github.com>
2023-03-25 15:55:53 +0530

    ./ added

--- C17-final-main/sketch.js
+++ C17-final-main/sketch.js
@@ -19,23 +19,23 @@ function preload(){
   trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
   trex_collided = loadAnimation("trex_collided.png");
   
-  groundImage = loadImage("ground2.png");
+  groundImage = loadImage("./ground2.png");
   
-  cloudImage = loadImage("cloud.png");
+  cloudImage = loadImage("./cloud.png");
   
-  obstacle1 = loadImage("obstacle1.png");
-  obstacle2 = loadImage("obstacle2.png");
-  obstacle3 = loadImage("obstacle3.png");
-  obstacle4 = loadImage("obstacle4.png");
-  obstacle5 = loadImage("obstacle5.png");
-  obstacle6 = loadImage("obstacle6.png");
+  obstacle1 = loadImage("./obstacle1.png");
+  obstacle2 = loadImage("./obstacle2.png");
+  obstacle3 = loadImage("./obstacle3.png");
+  obstacle4 = loadImage("./obstacle4.png");
+  obstacle5 = loadImage("./obstacle5.png");
+  obstacle6 = loadImage("./obstacle6.png");
   
-   restartImg = loadImage("restart.png")
-  gameOverImg = loadImage("gameOver.png")
+   restartImg = loadImage("./restart.png")
+  gameOverImg = loadImage("./gameOver.png")
   
-  jumpSound = loadSound("jump.mp3");
-  dieSound = loadSound("die.mp3");
-  checkPointSound = loadSound("checkPoint.mp3");
+  jumpSound = loadSound("./jump.mp3");
+  dieSound = loadSound("./die.mp3");
+  checkPointSound = loadSound("./checkPoint.mp3");
 }
 
 function setup() {
